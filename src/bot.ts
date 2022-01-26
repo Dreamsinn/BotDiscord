8be28376355245ab705ef30f5d6dbd66b107da59
XavierSantamaria <xaviersantamaria@DESKTOP-VTB04OL.localdomain>
2022-01-26 23:15:29 +0100

    bot created

---
+++ src/bot.ts
@@ -0,0 +1,18 @@
+// @ts-ignore
+
+const {Client, Intents} = require("discord.js");
+
+export class Bot {
+     static async CreateClient() {
+        // Create a new client instance
+        const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+        // When the client is ready, run this code (only once)
+        client.once('ready', () => {
+            console.log('Ready!');
+        });
+
+        // Login to Discord with your client's token
+         await client.login(process.env.TOKEN);
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+// Require the necessary discord.js classes
+import {Bot} from "./bot";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
+async function server () {
+    console.log('algo')
+    await Bot.CreateClient();
+    console.log('client created')
+}
+
+
+server();
+
 
