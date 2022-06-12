311f9e3a7a28e60998546bb212a7543e5369ccf6
XavierSantamaria <xaviersantamaria@DESKTOP-VTB04OL.localdomain>
2022-06-12 22:00:05 +0200

    fix bug: diceCommand, when rolling YDX, if Y was not number it rolled DX. And now says successes number if YDX </<=/>/>= Z

--- src/commands/domain/commandSchema/diceCommandSchema.ts
+++ src/commands/domain/commandSchema/diceCommandSchema.ts
@@ -4,7 +4,11 @@ const DiceCommandSchema: CommandSchema = {
     aliases: ['D'],
     coolDown: 120,
     devOnly: false,
-    description: `cuando YDX o DX, siendo Y e X numeros, es escrito en el chat se lanzan Y dados de X caras, tambien permite tirar mas de un dado a la vez, separado por ", ".\n Ejemplo: 2D5, 4D3`,
+    description:
+        `\`${process.env.PREFIX}roll on\` o \`${process.env.PREFIX}dice on\` para activar esta función, de la misma forma, off para desactivarla.` +
+        'Usos: \n' +
+        '- Tirar dados: escribir en el chat YDX o DX, siendo Y e X números, lanza Y dados de X caras. También  permite tirar más  de un dado a la vez, separado por ",".\n Ejemplo: 2D5, 4D3' +
+        '- Número de aciertos: se puede añadir a una tirada < o <= o > o >= y un número, para ver el número de aciertos de dicha tirada. No permite más de una tirada a la vez.\n Ejemplo: 4D6 > 3',
     category: 'dice',
     name: 'dice',
 };
--- src/commands/domain/commandSchema/diceCommandTogglerSchema.ts
+++ src/commands/domain/commandSchema/diceCommandTogglerSchema.ts
@@ -1,12 +1,12 @@
 import { CommandSchema } from '../interfaces/commandSchema';
 
 const DiceCommandTogglerSchema: CommandSchema = {
-    aliases: ['dice'],
+    aliases: ['dice', 'roll'],
     coolDown: 0,
     devOnly: false,
     description:
         'Activa o desactiva el comando de dados.\n' +
-        'Este comando debe ir seguido de ON o OFF\n' +
+        'Este comando debe ir seguido de ON u OFF\n' +
         `Egemplo: \n${process.env.PREFIX}dice on`,
     category: 'prefix',
     name: 'toggleDiceCommand',
