88ce325837bd5a530a36a76855b7c156972153fb
XavierSantamaria <xaviersantamaria@DESKTOP-VTB04OL.localdomain>
2022-09-24 14:20:45 +0200

    now DiceCommand and ReplyCommand don't use static methods

--- src/commands/domain/interfaces/Command.ts
+++ src/commands/domain/interfaces/Command.ts
@@ -1,6 +1,9 @@
 import { Message } from 'discord.js';
+import { DiceCommand } from '../../aplication/diceCommand';
+import { ReplyCommand } from '../../aplication/replyCommand';
 import { UsersUsingACommand } from '../../aplication/utils/usersUsingACommand';
 
 export abstract class Command {
-    abstract call(event: Message, userUsingCommand: UsersUsingACommand);
+    abstract call(event: Message, props?: UsersUsingACommand | DiceCommand | ReplyCommand);
 }
+
