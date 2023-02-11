6ce803f1132f9a48662b3c116aa38874ec83984c
XavierSantamaria <xavisantapo@gmail.com>
2023-02-11 17:43:33 +0100

    update PlayCommandSchema description

--- src/commands/domain/commandSchema/playCommandSchema.ts
+++ src/commands/domain/commandSchema/playCommandSchema.ts
@@ -8,16 +8,17 @@ const PlayCommandSchema: CommandSchema = {
     coolDown: 0,
     adminOnly: false,
     description:
-        'Este comando debe ir seguido de la canción que se quiere escuchar, hay que tener en cuenta que de momento solo coge la música de YouTube.\n\u200b\n' +
-        'Se le puede pasar como argumento:\n' +
+        'Este comando debe ir seguido de la canción que se quiere escuchar.\n' +
+        'Se le puede pasar como argumento:\n\u200b\n' +
         '-**Nombre de la cancion**: se puede buscar como si se estuviera buscando en la misma plataforma y elegir una de las primeras 9 opciones.\n' +
         '>  >__Durante este proceso no se podrán usar otros comandos.__\n\u200b\n' +
-        '-**Url de la canción**: la url de la canción de YouTube.\n\u200b\n' +
-        '-**Url playlist**: la url de la playlist de YouTube. En esta búsqueda hay que tener en cuenta:\n' +
+        '-**Url de la canción de Youtube**: tanto de móvil como de pc.\n\u200b\n' +
+        '-**Url playlist de Youtube**: En esta búsqueda hay que tener en cuenta:\n' +
         '>  >Si se estaba escuchando una canción de la playlist se preguntara si se desea escuchar la canción o la playlist.\n' +
         '>  >En el caso anterior, si se falla a conseguir la información de la playlist sonará la canción del enlace.\n' +
-        ">  >Las 'mix' de YouTube pueden fallar, por temas propios de YouTube.\n" +
-        '>  >Si la API principal falla, y se debe buscar por la propia de YouTube, tardará unos segundo, y como máximo cojera las 30 primeras canciones de la playlist.',
+        '-**Url de la canción de Spotify**.\n\u200b\n' +
+        '-**Url playlist de Spotify**.\n',
+
     command: CommandsNameEnum.PlayCommand,
     category: CommandsCategoryEnum.MUSIC,
 };
