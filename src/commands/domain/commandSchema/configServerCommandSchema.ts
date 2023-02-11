92dd109d08c1218b8c3cca3cf5ebebe13d65ddb5
XavierSantamaria <xavisantapo@gmail.com>
2023-02-11 17:35:32 +0100

    update ConfigServerCommand description

--- src/commands/domain/commandSchema/configServerCommandSchema.ts
+++ src/commands/domain/commandSchema/configServerCommandSchema.ts
@@ -7,7 +7,11 @@ export const ConfigServerCommand: CommandSchema = {
     aliases: ['config'],
     coolDown: 0,
     adminOnly: false,
-    description: '',
+    description:
+        'Este comando permite cambiar el prefijo en el servidor, el admin role del bot, y añadir y quitar usuarios a la blacklist.\n' +
+        'Hay que tener en cuenta:\n' +
+        '> - Durante el proceso el usuario no podrá usar otros comandos.' +
+        '> - Solo puede haber un admin role.',
     command: CommandsNameEnum.ConfigServerCommand,
     category: CommandsCategoryEnum.PREFIX,
 };
