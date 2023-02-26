b0986bbc3bd12170cb49647b0785bba245ee8244
XavierSantamaria <xavisantapo@gmail.com>
2023-02-26 22:44:48 +0100

    PlayListCommandSchema updated

--- src/commands/domain/commandSchema/playListCommandSchema.ts
+++ src/commands/domain/commandSchema/playListCommandSchema.ts
@@ -3,11 +3,12 @@ import { CommandsCategoryEnum } from '../enums/commandsCategoryEnum';
 import { CommandSchema } from '../interfaces/commandSchema';
 
 const PlayListCommandSchema: CommandSchema = {
-    name: 'PlayList',
+    name: 'Ver playList actual',
     aliases: ['playlist', 'pl'],
     coolDown: 0,
     adminOnly: false,
-    description: 'Muestra una lista paginada de todas las canciones de la playlist con su duracion.',
+    description:
+        'Muestra una lista paginada de todas las canciones, con su duracion, de la playlist que esta sonando.',
     command: CommandsNameEnum.PlaylistCommand,
     category: CommandsCategoryEnum.MUSIC,
 };
