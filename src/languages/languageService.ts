385efa61616584adec060cd7c9f8d93b37779f5e
xaviersantamaria <xavisantapo@gmail.com>
2023-08-14 00:32:16 +0200

    improve LanguageService error handling and make t sync

--- src/languages/languageService.ts
+++ src/languages/languageService.ts
@@ -4,8 +4,12 @@ export type Languages = 'es' | 'en';
 
 export const languagesArray: UnionToArray<Languages> = ['es', 'en'];
 
-class LanguageService {
-    private jsonDictionary: { [key in Languages]: any };
+export type JsonObject = {
+    [key: string]: JsonObject | string;
+};
+
+export class LanguageService {
+    private jsonDictionary: { [key in Languages]: JsonObject };
 
     constructor() {
         this.jsonDictionary = this.createJsonDictionary();
@@ -13,36 +17,53 @@ class LanguageService {
 
     private createJsonDictionary(): any {
         // create object with all languages
-        const jsonDictionary: { [key in Languages]?: any } = {};
+        const jsonDictionary: { [key in Languages]?: JsonObject } = {};
 
         languagesArray.forEach((lengauge: Languages) => {
-            jsonDictionary[lengauge] = import(`./locals/${lengauge}.json`);
+            jsonDictionary[lengauge] = require(`./locals/${lengauge}.json`);
         });
 
         return jsonDictionary;
     }
 
-    public async t(language: Languages, route: string, variables?: { [key: string]: string }) {
-        const json = await this.jsonDictionary[language];
+    public t(language: Languages, route: string, variables?: { [key: string]: string }) {
+        const json = this.jsonDictionary[language];
+
+        if (!json) {
+            return route;
+        }
 
         const routeSteps = route.split('.');
 
-        let step = json;
-        routeSteps.forEach((key: string) => {
-            step = step[key];
-        });
+        let response = this.getValueFromPath(routeSteps, json);
 
-        let response = `${step}`;
+        // if wront route, return it
+        if (!response) {
+            return route;
+        }
 
         if (!variables) {
             return response;
         }
 
         Object.entries(variables).forEach(([key, value]) => {
-            response = response.replace(`{{${key}}}`, value);
+            response = response!.replace(`{{${key}}}`, value);
         });
 
         return response;
+    }
+
+    private getValueFromPath(path: string[], obj: any): string | undefined {
+        let step = obj;
+        for (const key of path) {
+            if (step.hasOwnProperty(key)) {
+                step = step[key];
+            } else {
+                return undefined;
+            }
+        }
+
+        return step;
     }
 }
 
